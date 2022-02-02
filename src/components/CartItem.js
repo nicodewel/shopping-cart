dff0f13c83ec49930e925ea15e9707c41a4f48d9
Nicolas Weltzel <nwelt13@gmail.com>
2022-02-02 15:03:19 +0100

    deploy

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CartItem = ({ product, quantity }) => (
   <div className="list-group-item">
     <div className="row">
-      <div className="col-md-8">{product.name}</div>
+      <div className="col-md-8">{product.name}&#8623;</div>
       <div className="col-md-2">${product.priceInCents / 100}</div>
       <div className="col-md-2">{quantity}</div>
     </div>
